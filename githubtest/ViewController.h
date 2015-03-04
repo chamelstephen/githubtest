f5b1e99752591107db6a857eb58944e9f243eee3
kento <kento@Kentos-MacBook-Air.local>
2015-03-04 18:22:19 +0900

    Initial Commit

---
+++ githubtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubtest
+//
+//  Created by kento on 2015/03/04.
+//  Copyright (c) 2015年 Kento Ohara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ githubtest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  githubtest
+//
+//  Created by kento on 2015/03/04.
+//  Copyright (c) 2015年 Kento Ohara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
